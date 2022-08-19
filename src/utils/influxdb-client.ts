30e7a98a48a37350b95e3e9566cdd2d4218cecef
Thanh Hao <lthao1986@gmail.com>
2022-08-19 13:03:46 +0700

    Update coding write data to influxdb

--- src/utils/influxdb-client.ts
+++ src/utils/influxdb-client.ts
@@ -299,8 +299,8 @@ export class InfluxDBClient {
         .intField('price_change_24h', token.price_change_24h)
         .intField('price_change_percentage_24h', token.price_change_percentage_24h)
         .intField('total_volume', token.total_volume)
-        .intField('usd_24h_change', token.usd_24h_change)
         .intField('circulating_supply', token.circulating_supply)
+        .intField('max_supply', token.max_supply)
         .timestamp(this.convertDate(token.timestamp));
       points.push(point);
     });
