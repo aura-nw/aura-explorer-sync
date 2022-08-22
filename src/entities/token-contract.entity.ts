f582ca8980b5dfac0d93e1090f394265e2503b98
thub1310577 <thub1310577@gmail.com>
2022-08-22 14:12:27 +0700

    [Aura][ExplorerSync][Dev][ThuNPA] cw20 tokens list/detail token info

--- src/entities/token-contract.entity.ts
+++ src/entities/token-contract.entity.ts
@@ -34,4 +34,28 @@ export class TokenContract extends BaseEntityIncrementId {
 
   @Column({ name: 'coin_id' })
   coin_id: string;
+
+  @Column({ name: 'max_total_supply' })
+  max_total_supply: number;
+
+  @Column({ name: 'price' })
+  price: number;
+
+  @Column({ name: 'price_change_percentage_24h' })
+  price_change_percentage_24h: number;
+
+  @Column({ name: 'volume_24h' })
+  volume_24h: number;
+
+  @Column({ name: 'circulating_market_cap' })
+  circulating_market_cap: number;
+
+  @Column({ name: 'fully_diluted_market_cap' })
+  fully_diluted_market_cap: number;
+
+  @Column({ name: 'holders' })
+  holders: number;
+
+  @Column({ name: 'holders_change_percentage_24h' })
+  holders_change_percentage_24h: number;
 }
--- src/helpers/sync-data.helpers.ts
+++ src/helpers/sync-data.helpers.ts
@@ -474,7 +474,7 @@ export class SyncDataHelpers {
     return proposal;
   }
 
-  static makerCw20TokenData(item: any, marketingInfo: any) {
+  static makerCw20TokenData(item: any, marketingInfo: any, tokenInfo: any) {
     //sync data token
     const tokenContract = new TokenContract();
     tokenContract.type = CONTRACT_TYPE.CW20;
@@ -497,6 +497,16 @@ export class SyncDataHelpers {
     }
     tokenContract.num_tokens = 0;
     tokenContract.coin_id = '';
+    if (tokenInfo) {
+      tokenContract.max_total_supply = tokenInfo.max_supply;
+      tokenContract.price = tokenInfo.current_price;
+      tokenContract.price_change_percentage_24h = tokenInfo.price_change_percentage_24h;
+      tokenContract.volume_24h = tokenInfo.total_volume;
+      tokenContract.circulating_market_cap = tokenInfo.current_price * tokenInfo.circulating_supply;
+      tokenContract.fully_diluted_market_cap = tokenInfo.current_price * tokenInfo.max_supply;
+      // tokenContract.holders = tokenInfo.holders;
+      // tokenContract.holders_change_percentage_24h = tokenInfo.holders_change_percentage_24h;
+    }
     //sync data token owner
     const cw20TokenOwner = new Cw20TokenOwner();
     cw20TokenOwner.contract_address = item.contract_address;
