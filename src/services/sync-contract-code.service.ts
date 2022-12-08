2c23d9a1fea07c2a7332382e37a6ba27418a5eec
Thanh Hao <lthao1986@gmail.com>
2022-12-08 15:14:33 +0700

    Commit code

--- src/services/sync-contract-code.service.ts
+++ src/services/sync-contract-code.service.ts
@@ -65,44 +65,23 @@ export class SyncContractCodeService {
             )}`,
             '',
           );
-          switch (contractCodeIndexer.data.status) {
-            case CONTRACT_CODE_STATUS.COMPLETED:
-              item.result = CONTRACT_CODE_RESULT.CORRECT;
-              // //get contracts with code id
-              // const contractTypes: string[] = [
-              //   CONTRACT_TYPE.CW721,
-              //   CONTRACT_TYPE.CW20,
-              // ];
-              // if (contractTypes.includes(item.type)) {
-              //   const contractDB =
-              //     await this.smartContractRepository.findByCondition({
-              //       code_id: item.code_id,
-              //     });
-              //   if (contractDB && contractDB.length > 0) {
-              //     const contracts = [];
-              //     for (let i = 0; i < contractDB.length; i++) {
-              //       let contract: SmartContract = contractDB[i];
-
-              //       contract = await this._commonUtil.queryMoreInfoFromCosmwasm(
-              //         this.api,
-              //         contract.contract_address,
-              //         contract,
-              //         CONTRACT_TYPE[item.type],
-              //       );
-
-              //       contracts.push(contract);
-              //     }
-              //     await this.smartContractRepository.update(contracts);
-              //   }
-              // }
-              break;
-            case CONTRACT_CODE_STATUS.REJECTED:
-              item.result = CONTRACT_CODE_RESULT.INCORRECT;
-              break;
-            default:
-              item.result = CONTRACT_CODE_RESULT.TBD;
+          if (
+            contractCodeIndexer &&
+            contractCodeIndexer?.data.status !== 'NotFound'
+          ) {
+            item.type = contractCodeIndexer?.data?.contractType;
+            switch (contractCodeIndexer.data.status) {
+              case CONTRACT_CODE_STATUS.COMPLETED:
+                item.result = CONTRACT_CODE_RESULT.CORRECT;
+                break;
+              case CONTRACT_CODE_STATUS.REJECTED:
+                item.result = CONTRACT_CODE_RESULT.INCORRECT;
+                break;
+              default:
+                item.result = CONTRACT_CODE_RESULT.TBD;
+            }
+            contractCodes.push(item);
           }
-          contractCodes.push(item);
         }
         // update data
         await this.smartContractCodeRepository.update(contractCodes);
