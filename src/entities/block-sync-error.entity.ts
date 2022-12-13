3522d193c2d097c936747913b6ecd582e4087b54
nthduc95 <nthduc95@gmail.com>
2022-12-13 17:04:12 +0700

    Change data type id column to big int at table block_sync_error

--- src/entities/block-sync-error.entity.ts
+++ src/entities/block-sync-error.entity.ts
@@ -1,12 +1,14 @@
-import { Column, Entity, Unique } from 'typeorm';
-import { BaseEntityIncrementId } from './base/base.entity';
+import { Column, Entity, PrimaryGeneratedColumn, Unique } from 'typeorm';
+import { BaseEntity } from './base/base.entity';
 
 @Entity('block_sync_error')
 @Unique(['height'])
-export class BlockSyncError extends BaseEntityIncrementId {
-  @Column({ name: 'height' })
-  height: number;
+export class BlockSyncError extends BaseEntity {
 
-  @Column({ name: 'block_hash' })
-  block_hash: string;
+    @PrimaryGeneratedColumn('increment', { type: 'bigint' })
+    id: number;
+
+    @Column()
+    height: number;
+
 }
