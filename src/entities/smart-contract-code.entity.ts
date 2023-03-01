bf48ca22cf06154b3ec09ec594c38226477cf0e8
nthduc95 <nthduc95@gmail.com>
2023-03-01 10:16:26 +0700

    add entiry for contract code

--- src/entities/smart-contract-code.entity.ts
+++ src/entities/smart-contract-code.entity.ts
@@ -18,4 +18,36 @@ export class SmartContractCode extends BaseEntityIncrementId {
 
   @Column({ name: 'tx_hash' })
   tx_hash: string;
+
+  @Column({ name: 'instantiate_msg_schema', type: 'text' })
+  instantiate_msg_schema: string;
+
+  @Column({ name: 'query_msg_schema', type: 'text' })
+  query_msg_schema: string;
+
+  @Column({ name: 'execute_msg_schema', type: 'text' })
+  execute_msg_schema: string;
+
+  @Column({ name: 'contract_hash' })
+  contract_hash: string;
+
+  @Column({ name: 's3_location' })
+  s3_location: string;
+
+  @Column({
+    name: 'contract_verification',
+  })
+  contract_verification: string;
+
+  @Column({ name: 'compiler_version' })
+  compiler_version: string;
+
+  @Column({ name: 'url' })
+  url: string;
+
+  @Column({
+    type: 'timestamp',
+    name: 'verified_at',
+  })
+  verified_at: Date;
 }
