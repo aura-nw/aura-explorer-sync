32f7e2c67f0c45b91bcf16e83717cba20c18612e
nthduc95 <119287881+nthduc95@users.noreply.github.com>
2023-03-01 17:08:23 +0700

    Fix sync smart contract when verify contract (#371)

--- src/repositories/smart-contract-code.repository.ts
+++ src/repositories/smart-contract-code.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { SMART_CONTRACT_VERIFICATION } from '../common/constants/app.constant';
 import { SmartContractCode } from '../entities';
 import { BaseRepository } from './base.repository';
 
@@ -15,5 +16,31 @@ export class SmartContractCodeRepository extends BaseRepository<SmartContractCod
     this._logger.log(
       '============== Constructor Smart Contract Code Repository ==============',
     );
+  }
+
+  async findExactContractByHash(contract_hash: string) {
+    const query = this.repos
+      .createQueryBuilder('smart_contract_codes')
+      .where('smart_contract_codes.contract_hash = :contract_hash', {
+        contract_hash,
+      })
+      .andWhere(
+        'smart_contract_codes.contract_verification = :contract_verification',
+        {
+          contract_verification: SMART_CONTRACT_VERIFICATION.VERIFIED,
+        },
+      )
+      .select([
+        'smart_contract_codes.code_id as code_id',
+        'smart_contract_codes.url as url',
+        'smart_contract_codes.contract_verification as contract_verification',
+        'smart_contract_codes.compiler_version as compiler_version',
+        'smart_contract_codes.instantiate_msg_schema as instantiate_msg_schema',
+        'smart_contract_codes.query_msg_schema as query_msg_schema',
+        'smart_contract_codes.execute_msg_schema as execute_msg_schema',
+        'smart_contract_codes.s3_location as s3_location',
+      ]);
+    const res = await query.getRawOne();
+    return res;
   }
 }
